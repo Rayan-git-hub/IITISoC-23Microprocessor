2a8a204183a002c60a3af29698ad19735dd3ce3a
pgoel02 <108073615+pgoel02@users.noreply.github.com>
2023-06-29 19:28:54 +0530

    Create ALU_tb.v

---
+++ `timescale 1ns /ALU_tb.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+
+
+module alu_tb( );
+reg [7:0] in1,in2;
+reg clk=0;
+reg [2:0] opcode;
+wire [7:0] out;
+ALU_design  a(in1,in2,opcode,out);
+
+initial 
+begin
+#20 in1=8'b00000000;in2=8'b00000000;opcode=3'b000;
+#20 opcode=3'b010;
+#20 in1=8'b00000001;in2=8'b00000001;opcode=3'b000;
+#20 opcode=3'b001;
+#20 in1=8'b00001000;in2=8'b00000000;opcode=3'b011;
+#20 $finish();
+end
+endmodule
